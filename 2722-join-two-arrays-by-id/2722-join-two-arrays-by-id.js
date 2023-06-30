99932bda7ee71dd50ed13b955c44ccdc253f2242
devstev2022 <devstev2022@gmail.com>
2023-06-30 01:57:24 -0500

    Time: 355 ms (71.85%), Space: 115.6 MB (53.46%) - LeetHub

--- 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
+++ 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
@@ -10,7 +10,7 @@ var join = function(arr1, arr2) {
     } 
     for (let i = 0; i < arr2.length; i++) {
         if (result[arr2[i].id]) {
-            for (const key in arr2[i]) result[arr2[i].id][key] = arr2[i][key];  
+            result[arr2[i].id] = {...result[arr2[i].id], ...arr2[i]}; 
         } else {
             result[arr2[i].id] = arr2[i];
         }
