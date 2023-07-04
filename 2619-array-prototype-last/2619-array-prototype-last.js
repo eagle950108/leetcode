7cf945983fbae674609ff61d8301ba8dc547d61a
devstev2022 <devstev2022@gmail.com>
2023-07-04 00:10:09 -0500

    Time: 60 ms (38.92%), Space: 41.5 MB (81.09%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -1,6 +1,7 @@
 Array.prototype.last = function() {
-    if(this.length === 0) return -1;
-    return this[this.length - 1];
+    const length = this.length;
+    if(length === 0) return -1;
+    return this[length - 1];
 };
 
 /**
