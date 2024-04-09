364ea1cf5fc5ecc4e15c08728abe2ae9ca9488bd
eagle950108 <devstev2022@gmail.com>
2024-04-09 22:37:14 +0200

    Time: 39 ms (97.00%), Space: 16.5 MB (64.90%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,17 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        list1 = []
-        list2 = []
-        if x < 0:
-            return False
-        else:
-            while(x):
-                num = x % 10
-                x = (int)((x - num) / 10)
-                list1.append(num)
-            list2 = list1[::-1]
-            if list1 == list2:
-                return True
-            else:
-                return False
+        str_x = str(x)
+        return str_x == str_x[::-1]
             
