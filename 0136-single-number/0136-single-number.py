ab6f8b056877573947c075ac5a2d7db7bd99131e
eagle950108 <devstev2022@gmail.com>
2024-04-22 14:49:13 -0700

    Time: 85 ms (96.29%), Space: 14 MB (40.44%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        result = 0
+        
         for num in nums:
-            if nums.count(num) == 1:
-                return num
+            result ^= num
+        
+        return result
